ce215a464411a7ae276e8ba4a07c03040dc329dd
郭鹏飞 <guopengfei@cloudin.kmail.com>
2017-09-30 14:25:44 +0800

    test updateStateBykey

--- src/main/scala/com/feixeyes/realetime/SocketCosumeDemo.scala
+++ src/main/scala/com/feixeyes/realetime/SocketCosumeDemo.scala
@@ -13,16 +13,27 @@ class SocketCosumeDemo {
 }
 
 object SocketCosumeDemo {
+  def updateFunction(newValues: Seq[Int], runningCount: Option[Int]): Option[Int] = {
+    var sum = 0
+    if( newValues !=None && newValues.length != 0){
+      sum = newValues.reduce((x,y)=>x+y)
+    }
+    sum += runningCount.getOrElse(0)
+    Some(sum)
+  }
+
   def main(args:Array[String] ){
 
     Logger.getLogger("org").setLevel(Level.ERROR)
     val sc = new SparkConf().setAppName("socketStreaming")
     val ssc = new StreamingContext(sc, Seconds(3))
+    ssc.checkpoint("/Users/apple/test")
     val lines = ssc.socketTextStream("localhost", 9999)
 
     lines.flatMap( _.split(" "))
       .map(word => (word,1))
       .reduceByKey(_ + _)
+      .updateStateByKey(updateFunction)
       .print()
     ssc.start()
     ssc.awaitTermination()
